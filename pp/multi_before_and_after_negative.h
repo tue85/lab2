5d04f8a7ef4c5c007a5b8242d0e9e8b9c408d7f0
Anisimov Dmitriy <144445957+tue85@users.noreply.github.com>
2023-11-29 13:55:26 +0300

    Delete pp directory

--- pp/multi_before_and_after_negative.h
+++
@@ -1,6 +0,0 @@
-#ifndef MULTI_BEFORE_AND_AFTER_NEGATIVE_H
-#define MULTI_BEFORE_AND_AFTER_NEGATIVE_H
-
-int multi_before_and_after_negative(int x[],int n,int index_1,int index_2);
-
-#endif
--- pp/multi_between_negative.h
+++
@@ -1,6 +0,0 @@
-#ifndef MULTI_BETWEEN_NEGATIVE_H
-#define MULTI_BETWEEN_NEGATIVE_H
-
-int multi_between_negative(int x[],int n,int index_1,int index_2);
-
-#endif
